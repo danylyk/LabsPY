151bb30d65f8a9bb2655e0c8c541465b444586c6
danylyk <web.vitaly@rambler.ru>
2019-10-24 15:27:22 +0300

    Update Lab 5

--- Lab5.py
+++ Lab5.py
@@ -8,12 +8,20 @@ class Person:
         print("Hello, my name is " + self.name)
 
     #def Save (self):
-    #    pass
+    #    print("Saved")
 
 class DB:
-    def Save (person: Person):
-        pass
+    def Save (self, person: Person):
+        print(str(person.name) + " saved")
 
+if __name__ == "__main__":
+    db = DB()
+    ivan = Person("Ivan")
+    julia = Person("Julia")
+    db.Save(ivan)
+    db.Save(julia)
+
+    print("---")
 
 
 #OCP
@@ -34,6 +42,14 @@ class Circle(IArea):
     def Area(self):
         return 3.14*self.radius**2
 
+if __name__ == "__main__":
+    square = Square(10)
+    circle = Circle(10)
+    print(square.Area())
+    print(circle.Area())
+
+    print("---")
+
 
 #LSP
 
@@ -49,8 +65,16 @@ class Dodge(Auto):
     def Power(self):
         return 160
 
-def GetPower (car):
-    car.Power()
+def GetPower (car: Auto):
+    return car.Power()
+
+if __name__ == "__main__":
+    car1 = Lexus()
+    car2 = Dodge()
+    print(GetPower(car1))
+    print(GetPower(car2))
+
+    print("---")
 
 
 #ISP
@@ -75,12 +99,6 @@ def GetPower (car):
 #        #code
 #        pass
 
-
-class Triangle(ITriangle):
-    def DrawTriangle(self):
-        #code
-        pass
-
 class ICircle:
     def DrawCircle(self):
         pass
@@ -91,13 +109,21 @@ class ITriangle:
 
 class Circle(ICircle):
     def DrawCircle(self):
-        #code
+        print("Draw Circle")
         pass
 
 class Triangle(ITriangle):
     def DrawTriangle(self):
-        #code
+        print("Draw Triangle")
         pass
+
+if __name__ == "__main__":
+    circle = Circle()
+    triangle = Triangle()
+    circle.DrawCircle()
+    triangle.DrawTriangle()
+
+    print("---")
 
 
 
@@ -109,11 +135,11 @@ class Idb:
 
 class MySQLDB(Idb):
     def Connect(self):
-        pass
+        print("MySQL DB is connected")
 
 class MongoDB(Idb):
     def Connect(self):
-        pass
+        print("MongoDB is connected")
 
 class Books:
     def __init__ (self, db: Idb):
@@ -121,8 +147,10 @@ class Books:
 
     def GetBooks (self):
         conn = self.db.Connect()
+        print("List of books")
 
-mysql = MySQLDB()
-mongo = MongoDB()
-b1 = Books(mysql).GetBooks()
-b2 = Books(mongo).GetBooks()
+if __name__ == "__main__":
+    mysql = MySQLDB()
+    mongo = MongoDB()
+    b1 = Books(mysql).GetBooks()
+    b2 = Books(mongo).GetBooks()
