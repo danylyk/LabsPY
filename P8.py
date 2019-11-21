bc572ba66ba1cfe7eda4fde4a35d008a88dd4035
danylyk <web.vitaly@rambler.ru>
2019-11-22 01:11:23 +0200

    Lab 8 Update

--- P8.py
+++ P8.py
@@ -1,23 +1,30 @@
 import os
 
-class Directory:
+class FilesInDirectory:
     def __init__ (self, dir:str):
         self.dir = dir
 
     def files_list (self):
-        return os.listdir(self.dir)
+        return [a for a in self.files_from_dir(self.dir)]
+
+    def files_from_dir (self, dir):
+        for item in os.listdir(dir):
+            if os.path.isdir(dir+item):
+                yield from self.files_from_dir(dir+item+"/")
+            else:
+                yield dir+item
 
     def get_duplicates (self):
         files = self.files_list()
         res = []
         i = 0
         while i < len(files):
-            with open(self.dir+files[i], 'rb') as file:
+            with open(files[i], 'rb') as file:
                 f1 = file.readlines()
             found = False
             j = i+1
             while j < len(files):
-                with open(self.dir+files[j], 'rb') as file:
+                with open(files[j], 'rb') as file:
                     f2 = file.readlines()
                 if (str(f1) == str(f2)):
                     if found == False:
@@ -32,5 +39,5 @@ class Directory:
         return res
 
 if __name__ == '__main__':
-    dir = Directory("C:/Users/webvi/OneDrive/Робочий стіл/P8/")
+    dir = FilesInDirectory("C:/Users/webvi/OneDrive/Робочий стіл/P8/")
     print(dir.get_duplicates())
