b54eccb35654ebcbe58e1d3734b188248986c1c8
danylyk <web.vitaly@rambler.ru>
2019-11-22 02:22:49 +0200

    Lab 7 Update

--- Lab7.py
+++ Lab7.py
@@ -1,12 +1,25 @@
 import urllib.request
+import re
 
-def GetWordCount (url:str, word:str):
+def GetWordsCount (url:str):
   #res = urllib.request.urlretrieve(url, 'File.txt') 
-  res = urllib.request.urlopen(url)
-  res = res.read().decode("utf-8")
-  res = res.split(word)
-  return len(res)-1
+  data = urllib.request.urlopen(url)
+  data = data.read().decode("utf-8").lower().replace("\'", "")
+  data = re.findall('(\w+(?:-\w+)+|\w+)', data)
+  res = [[data[0], 1]]
+  for i in range(len(data)):
+    new = True
+    for j in range(len(res)):
+      if res[j][0] == data[i]:
+        res[j][1] += 1
+        new = False
+        break
+    if new:
+      res.append([data[i], 1])
+
+  return res
 
 if __name__ == "__main__":
-  words = GetWordsCount("https://borscht.mobi/SimpleFile.txt", "ipsum")
-  print(words)
+  words = GetWordsCount("https://www.sample-videos.com/text/Sample-text-file-100kb.txt")
+  for item in words:
+    print(item[0]+": "+str(item[1])) 
