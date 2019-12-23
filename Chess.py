ebcf38e9650a63864dd8011bb2b9b33b36b0f576
danylyk <web.vitaly@rambler.ru>
2019-12-23 22:34:18 +0200

    It should be good

--- Chess.py
+++ Chess.py
@@ -10,10 +10,13 @@ from bs4 import BeautifulSoup
 class EventNotifier:
     def __init__ (self, emails):
         self.emails = emails
+        self.participants = {}
+        self.tournaments = {}
         self.last_updated = ""
         self.domain = "http://chess-results.com/"
         self.data = []
         self.app_run = True
+        self.appending = True
         self.data_update = False
         if os.path.isfile("data.dv"):
             with open('data.dv') as saved_data:
@@ -33,8 +36,69 @@ class EventNotifier:
             json.dump(self.data, saved_data)
 
     def run (self):
+        self.appending = False
         while self.app_run:
-            self.get_update()
+            #self.get_update()
+            for url in self.participants:
+                self.get_patricipant_update(url)
+            for url in self.tournaments:
+                self.tournament_update(url)
+
+    def append_participant (self, url):
+        player = {"name": "Unknown", "rating": "0", "points": "0", "rank": "0", "rd": "1"}
+
+        r = requests.get(url)
+        html_content = r.text
+
+        soup = BeautifulSoup(html_content, "html.parser")
+
+        data = soup.find("td", text="Name")
+        if (data != None):
+            data = data.find_parent("tr").find("td")
+            if len(data) > 1:
+                player["name"] = data[1].text
+
+        data = soup.find("td", text="Rating")
+        if (data != None):
+            data = data.find_parent("tr").find("td")
+            if len(data) > 1:
+                player["rating"] = data[1].text
+
+        data = soup.find("td", text="Points")
+        if (data != None):
+            data = data.find_parent("tr").find("td")
+            if len(data) > 1:
+                player["points"] = data[1].text
+
+        data = soup.find("td", text="Rank")
+        if (data != None):
+            data = data.find_parent("tr").find("td")
+            if len(data) > 1:
+               player["rank"] = data[1].text
+
+        data = soup.find_all("table", {"class": "CRs1"})
+        if len(data) > 2:
+            data = data[1]
+            participant_tr = data.find_all("tr")
+            if len(participant_tr) > 0:
+                participant_tr = participant_tr[-1]
+                participant = participant_tr.find_all("td")
+                if len(participant) > 0:
+                    player["rd"] = participant[0].text
+
+        self.participants[url] = player
+
+    def append_tournament (self, url):
+        r = requests.get(url)
+        html_content = r.text
+        soup = BeautifulSoup(html_content, "html.parser")
+        title = soup.find_all("div", {"id": "_ctl0_F7"})[0].find("h2")
+        if title == None:
+            title = "Unknown"
+        else:
+            title = title.text
+        self.tournament_update(url, title)
+
 
     def get_update (self):        
         r = requests.get(self.domain)
@@ -48,17 +112,61 @@ class EventNotifier:
             self.last_updated = updated.text
             self.tournament_update(updated.find_all("a")[0]['href'], self.last_updated)
 
+    def get_patricipant_update (self, url):        
+        r = requests.get(url)
+        html_content = r.text
+
+        soup = BeautifulSoup(html_content, "html.parser")
+
+        data = soup.find("td", text="Rating")
+        if (data != None):
+            data = data.find_parent("tr").find("td")
+            if len(data) > 1:
+                if self.participants[url]["rating"] != data[1].text:
+                    self.send_email("Rating ("+self.participants[url]["name"]+") was updated. New rating: "+data[1].text)
+                    self.participants[url]["rating"] = data[1].text
+
+        data = soup.find("td", text="Points")
+        if (data != None):
+            data = data.find_parent("tr").find("td")
+            if len(data) > 1:
+                if self.participants[url]["points"] != data[1].text:
+                    self.send_email("Points ("+self.participants[url]["name"]+") was updated. New points: "+data[1].text)
+                    self.participants[url]["points"] = data[1].text
+
+        data = soup.find("td", text="Rank")
+        if (data != None):
+            data = data.find_parent("tr").find("td")
+            if len(data) > 1:
+               if self.participants[url]["rank"] != data[1].text:
+                    self.send_email("Rank ("+self.participants[url]["name"]+") was updated. New rank: "+data[1].text)
+                    self.participants[url]["rank"] = data[1].text
+
+        data = soup.find_all("table", {"class": "CRs1"})
+        if len(data) > 2:
+            data = data[1]
+            participant_tr = data.find_all("tr")
+            if len(participant_tr) > 0:
+                participant_tr = participant_tr[-1]
+                participant = participant_tr.find_all("td")
+                if len(participant) > 0:
+                    if self.participants[url]["rd"] != participant[0].text:
+                        self.send_email("Rd. ("+self.participants[url]["name"]+") was updated. New rd.: "+data[1].text)
+                        self.participants[url]["rd"] = participant[0].text
+
     def check_tournament (self, link, title, data):
         for item in self.data:
             if item["link"] == link:
                 update = False
                 if item["data"][0][1] != data[0][1]:
-                    self.send_email("Tournament \""+title+"\" was updated. New tour started: "+data[0][1])
+                    if self.appending == False:
+                        self.send_email("Tournament \""+title+"\" was updated. New tour started: "+data[0][1])
                     item["data"][0][1] = data[0][1]
                     item["data"][0][0] = data[0][0]
                     update = True
                 if item["data"][1][0] != data[1][0]:
-                    self.send_email("Tournament \""+title+"\" was updated. New participant added: "+data[1][1])
+                    if self.appending == False:    
+                        self.send_email("Tournament \""+title+"\" was updated. New participant added: "+data[1][1])
                     item["data"][1][0] = data[1][0]
                     item["data"][1][1] = data[1][1]
                     update = True
@@ -80,7 +188,7 @@ class EventNotifier:
         s.quit()
     
     def tournament_update (self, link, title):
-        r = requests.get(self.domain+link+"&zeilen=99999")
+        r = requests.get(link+"&zeilen=99999")
         html_content = r.text
 
         soup = BeautifulSoup(html_content, "html.parser")
--- Client.py
+++ Client.py
@@ -3,4 +3,7 @@ from Chess import EventNotifier
 if __name__ == "__main__":
     emails = ["webdvitaly@gmail.com", "web.vitaly@rambler.ru"]
     notifier = EventNotifier(emails)
+    notifier.append_participant("http://chess-results.com/tnr83447.aspx?lan=1&art=9&snr=23")
+    notifier.append_participant("http://chess-results.com/tnr149322.aspx?lan=1&art=9&snr=118")
+    notifier.append_tournament("http://chess-results.com/tnr219320.aspx?lan=1")
     notifier.run()
